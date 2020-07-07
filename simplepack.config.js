7d4f8ac24826982b49e52c24055d6e4db43701ad
sunxiaojie79 <297280911@qq.com>
2020-07-08 01:01:38 +0800

    feat:simple-pack

--- simplepack.config.js
+++ simplepack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, 'src'),
+  entry: path.join(__dirname, 'src/index.js'),
   output: {
     filename: 'main.js',
     path: path.join(__dirname, 'dist')
